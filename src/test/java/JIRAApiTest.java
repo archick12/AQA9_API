e411ca128bf660d03a1607449b57f3b9d8de99bb
apiliuk <lohika.artur@98point6.com>
2020-09-02 21:11:50 +0300

    create issue

--- src/test/java/JIRAApiTest.java
+++ src/test/java/JIRAApiTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import io.restassured.http.Cookies;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseOptions;
@@ -17,27 +18,64 @@ public class JIRAApiTest {
     public void getExistingIssue() {
 
         Response response =
-
-                given().
-                        auth().preemptive().basic("webinar5", "webinar5").
-                        when().
-                        get("http://jira.hillel.it/rest/api/2/issue/WEBINAR-9060").
+                        given().
+                            auth().preemptive().basic("webinar5", "webinar5").
+                            contentType(ContentType.JSON).
+                        when().get("http://jira.hillel.it/rest/api/2/issue/WEBINAR-9060").
                         then().
-                        extract().response();
+                            contentType(ContentType.JSON).
+                            statusCode(200).
+                            extract().response();
 
-        Cookies coockies = response.getDetailedCookies();
-
-        assertEquals(response.statusCode(), 200);
-        assertEquals("WEBINAR-9060", response.path("key"));
-        final Matcher<String> matcher = new MatchesPattern(Pattern.compile("[A-Z]+\\-[0-9]+"));
-        assertTrue(matcher.matches("WEBINAR-9060"));
+        // assertEquals(response.statusCode(), 200);
+        // assertEquals(response.contentType(), ContentType.JSON.toString());
+        assertEquals("WEBINAR-9060", response.path("key")); // JSON Path syntax
     }
 
+    @Test
+    public void createIssueRaw(){
+
+                given().
+                    auth().preemptive().basic("webinar5", "webinar5").
+                    contentType(ContentType.JSON).
+                    body("{\n" +
+                        "   \"fields\":{\n" +
+                        "      \"summary\":\"Main order flow broken\",\n" +
+                        "      \"issuetype\":{\n" +
+                        "         \"id\":\"10105\",\n" +
+                        "         \"name\":\"test\"\n" +
+                        "      },\n" +
+                        "      \"project\":{\n" +
+                        "         \"id\":\"10508\"\n" +
+                        "      },\n" +
+                        "   \"reporter\": {\n" +
+                        "      \"name\": \"webinar5\"\n" +
+                        "    }\n" +
+                        "   }\n" +
+                        "}\u2029").
+                when().
+                    post("https://jira.hillel.it/rest/api/2/issue").
+                then().
+                    contentType(ContentType.JSON).
+                    statusCode(201).
+                    extract().response().print();
+    }
 
     @Test
     public void createIssue() {
         Response response = JiraAPISteps.createIssue();
         assertEquals(201, response.statusCode());
         response.then().extract().path("key");
+        // TODO check that id matches pattern
     }
 }
+
+
+
+
+
+//        Cookies coockies = response.getDetailedCookies();
+
+//        final Matcher<String> matcher = new MatchesPattern(Pattern.compile("[A-Z]+\\-[0-9]+"));
+//        assertTrue(matcher.matches("WEBINAR-9060"));
+// TODO preemptive - что дает?
